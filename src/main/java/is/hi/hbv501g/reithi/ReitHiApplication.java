9fce7ace1bd0fa1a06d8d434860c49bad65b986f
JonasHakon <jonashakon01@gmail.com>
2022-09-06 12:04:07 +0000

    First commit

--- src/main/java/is/hi/hbv501g/reithi/ReitHiApplication.java
+++ src/main/java/is/hi/hbv501g/reithi/ReitHiApplication.java
@@ -1,10 +1,12 @@
 package is.hi.hbv501g.reithi;
 
+// this is team member Jónas Hákon Kjartansson
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ReitHiApplication {
 
     public static void main(String[] args) {
