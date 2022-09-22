1036031731512d1b2633571d1d51837b1a50bf6a
tomaskri <98184013+tomaskri@users.noreply.github.com>
2022-09-22 10:30:53 +0000

    Basic classes added

---
+++ src/main/java/is/hi/hbv501g/reithi/Course.java
@@ -0,0 +1,4 @@
+package is.hi.hbv501g.reithi;
+
+public class Course {
+}
---
+++ src/main/java/is/hi/hbv501g/reithi/HelloController.java
@@ -0,0 +1,14 @@
+package is.hi.hbv501g.reithi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
