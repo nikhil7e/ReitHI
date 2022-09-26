924b9c497ac69dab6d5ef0b7615d7cd64f9e4876
Nikhil Kumar <nikhil7e@gmail.com>
2022-09-26 14:10:50 +0000

    Add postgres dependency

--- src/main/java/is/hi/hbv501g/reithi/Services/Implementation/CourseServiceImplementation.java
+++ src/main/java/is/hi/hbv501g/reithi/Services/Implementation/CourseServiceImplementation.java
@@ -1,9 +1,35 @@
 package is.hi.hbv501g.reithi.Services.Implementation;
 
 import is.hi.hbv501g.reithi.Persistence.Entities.Course;
+import is.hi.hbv501g.reithi.Services.CourseService;
 
-public class CourseServiceImplementation {
+import java.util.List;
+
+public class CourseServiceImplementation implements CourseService {
 
 
+    @Override
+    public Course findByName(String name) {
+        return null;
+    }
 
+    @Override
+    public List<Course> findAll() {
+        return null;
+    }
+
+    @Override
+    public Course findByID(long ID) {
+        return null;
+    }
+
+    @Override
+    public Course save(Course course) {
+        return null;
+    }
+
+    @Override
+    public void delete(Course course) {
+
+    }
 }
