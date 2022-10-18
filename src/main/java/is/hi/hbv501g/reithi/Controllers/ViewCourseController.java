f434a6d6d429720e6e212f83c612d0382cf3c6df
tomaskri <98184013+tomaskri@users.noreply.github.com>
2022-10-18 22:35:24 +0000

    Added ability to read comments

--- src/main/java/is/hi/hbv501g/reithi/Controllers/ViewCourseController.java
+++ src/main/java/is/hi/hbv501g/reithi/Controllers/ViewCourseController.java
@@ -1,20 +1,28 @@
 package is.hi.hbv501g.reithi.Controllers;
 
+import is.hi.hbv501g.reithi.Persistence.Entities.Course;
+import is.hi.hbv501g.reithi.Persistence.Entities.Review;
+import is.hi.hbv501g.reithi.Persistence.Repositories.CourseRepository;
 import is.hi.hbv501g.reithi.Services.CourseService;
+import is.hi.hbv501g.reithi.Services.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.List;
+
 @Controller
 public class ViewCourseController {
 
     private CourseService courseService;
+    private ReviewService reviewService;
 
     @Autowired
-    public ViewCourseController(CourseService courseService) {
+    public ViewCourseController(CourseService courseService, ReviewService reviewService) {
         this.courseService = courseService;
+        this.reviewService = reviewService;
     }
 
     @RequestMapping(value = "/reviewcourse", method = RequestMethod.GET)
