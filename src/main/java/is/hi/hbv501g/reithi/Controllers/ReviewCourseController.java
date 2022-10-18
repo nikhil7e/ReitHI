b24181c97a4046e1c1791dc626650719cf929fd4
tomaskri <98184013+tomaskri@users.noreply.github.com>
2022-10-18 22:03:23 +0000

    Finalizing requirement 2

--- src/main/java/is/hi/hbv501g/reithi/Controllers/ReviewCourseController.java
+++ src/main/java/is/hi/hbv501g/reithi/Controllers/ReviewCourseController.java
@@ -2,11 +2,11 @@ package is.hi.hbv501g.reithi.Controllers;
 
 import is.hi.hbv501g.reithi.Persistence.Entities.Comment;
 import is.hi.hbv501g.reithi.Persistence.Entities.Course;
-import is.hi.hbv501g.reithi.Persistence.Entities.Course;
 import is.hi.hbv501g.reithi.Persistence.Entities.Rating;
 import is.hi.hbv501g.reithi.Persistence.Entities.Review;
 import is.hi.hbv501g.reithi.Services.CourseService;
 import is.hi.hbv501g.reithi.Services.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -21,6 +21,12 @@ public class ReviewCourseController {
     private CourseService courseService;
     private ReviewService reviewService;
 
+    @Autowired
+    public ReviewCourseController(CourseService courseService, ReviewService reviewService) {
+        this.courseService = courseService;
+        this.reviewService = reviewService;
+    }
+
     // TODO: For testing purposes only, remove eventually
     @RequestMapping(value = "/addreview", method = RequestMethod.POST)
     public String addReviewPOST(Comment comment, Rating rating, BindingResult result, Model model, HttpSession session) {
@@ -29,8 +35,8 @@ public class ReviewCourseController {
         }
 
         reviewService.save(new Review(null, rating, comment, (Course) session.getAttribute("selectedCourse")));
-        Course c = (Course) session.getAttribute("selectedCourse");
-        //courseService.save(course);
+        //Course c = (Course) session.getAttribute("selectedCourse");
+        //courseService.save(c);
         return "landingPage";
     }
 
