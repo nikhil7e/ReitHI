dd71d2e5136e0e36e5b8b3f152d7dcb6310b2c3c
Nikhil Kumar <nikhil7e@gmail.com>
2022-11-12 21:52:26 +0000

    Add javadoc and spring devtools dependency

--- src/main/java/is/hi/hbv501g/reithi/Controllers/CourseSearchResultsController.java
+++ src/main/java/is/hi/hbv501g/reithi/Controllers/CourseSearchResultsController.java
@@ -16,6 +16,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import javax.servlet.http.HttpSession;
 import java.util.List;
 
+/**
+ * This controller handles HTTP requests for the course search results page, such as selecting
+ * a course to be viewed.
+ */
 @Controller
 public class CourseSearchResultsController {
 
@@ -53,6 +57,15 @@ public class CourseSearchResultsController {
         return "searchResults";
     }
 
+    /**
+     * Gets the ViewCourse template and sets model and session attributes
+     * to course details that will be displayed
+     *
+     * @param id      The id of the course to be viewed
+     * @param model   The applications model
+     * @param session The applications session
+     * @return the ViewCourse template
+     */
     @RequestMapping(value = "/viewcourse/{id}", method = RequestMethod.GET)
     public String viewCourseGET(@PathVariable("id") long id, Model model, HttpSession session) {
         // update html to use session instead of model and delete line 43
@@ -60,17 +73,17 @@ public class CourseSearchResultsController {
         // Get course rating from selected course
         Course selectedCourse = (Course) model.getAttribute("selectedCourse");
         // Include rating data course in HTML
-        model.addAttribute("avgOAS",reviewService.getAverageOverallScore(selectedCourse.getID()));
-        model.addAttribute("avgD",reviewService.getAverageDifficulty(selectedCourse.getID()));
-        model.addAttribute("avgW",reviewService.getAverageWorkload(selectedCourse.getID()));
-        model.addAttribute("avgTQ",reviewService.getAverageTeachingQuality(selectedCourse.getID()));
-        model.addAttribute("avgCM",reviewService.getAverageCourseMaterial(selectedCourse.getID()));
+        model.addAttribute("avgOAS", reviewService.getAverageOverallScore(selectedCourse.getID()));
+        model.addAttribute("avgD", reviewService.getAverageDifficulty(selectedCourse.getID()));
+        model.addAttribute("avgW", reviewService.getAverageWorkload(selectedCourse.getID()));
+        model.addAttribute("avgTQ", reviewService.getAverageTeachingQuality(selectedCourse.getID()));
+        model.addAttribute("avgCM", reviewService.getAverageCourseMaterial(selectedCourse.getID()));
         session.setAttribute("selectedCourse", courseService.findByID(id));
-        session.setAttribute("avgOAS",reviewService.getAverageOverallScore(selectedCourse.getID()));
-        session.setAttribute("avgD",reviewService.getAverageDifficulty(selectedCourse.getID()));
-        session.setAttribute("avgW",reviewService.getAverageWorkload(selectedCourse.getID()));
-        session.setAttribute("avgTQ",reviewService.getAverageTeachingQuality(selectedCourse.getID()));
-        session.setAttribute("avgCM",reviewService.getAverageCourseMaterial(selectedCourse.getID()));
+        session.setAttribute("avgOAS", reviewService.getAverageOverallScore(selectedCourse.getID()));
+        session.setAttribute("avgD", reviewService.getAverageDifficulty(selectedCourse.getID()));
+        session.setAttribute("avgW", reviewService.getAverageWorkload(selectedCourse.getID()));
+        session.setAttribute("avgTQ", reviewService.getAverageTeachingQuality(selectedCourse.getID()));
+        session.setAttribute("avgCM", reviewService.getAverageCourseMaterial(selectedCourse.getID()));
 
         List<Review> reviewSearchResults = reviewService.findByCourse_Name(((Course) model.getAttribute("selectedCourse")).getName());
         session.setAttribute("reviewsForCourse", reviewSearchResults);
--- src/main/java/is/hi/hbv501g/reithi/Controllers/ReviewCourseController.java
+++ src/main/java/is/hi/hbv501g/reithi/Controllers/ReviewCourseController.java
@@ -3,7 +3,6 @@ package is.hi.hbv501g.reithi.Controllers;
 import is.hi.hbv501g.reithi.Persistence.Entities.*;
 import is.hi.hbv501g.reithi.Services.CourseService;
 import is.hi.hbv501g.reithi.Services.ReviewService;
-import is.hi.hbv501g.reithi.Services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import javax.servlet.http.HttpSession;
 import java.util.List;
 
+/**
+ * This controller handles HTTP requests for review functionality, such as adding and deleting reviews.
+ */
 @Controller
 public class ReviewCourseController {
 
@@ -26,7 +28,16 @@ public class ReviewCourseController {
         this.reviewService = reviewService;
     }
 
-    // TODO: For testing purposes only, remove eventually
+    /**
+     * Creates a review and saves it in the database. Redirects to the course view page with updated values.
+     *
+     * @param comment The reviews optional comment
+     * @param rating  The reviews rating
+     * @param result  The binding result
+     * @param model   The applications model
+     * @param session The applications session
+     * @return The course view page template
+     */
     @RequestMapping(value = "/addreview", method = RequestMethod.POST)
     public String addReviewPOST(Comment comment, Rating rating, BindingResult result, Model model, HttpSession session) {
         if (result.hasErrors()) {
@@ -36,11 +47,11 @@ public class ReviewCourseController {
         reviewService.save(new Review((User) session.getAttribute("LoggedInUser"), rating, comment, (Course) session.getAttribute("selectedCourse")));
 
         long id = ((Course) session.getAttribute("selectedCourse")).getID();
-        session.setAttribute("avgOAS",reviewService.getAverageOverallScore(id));
-        session.setAttribute("avgD",reviewService.getAverageDifficulty(id));
-        session.setAttribute("avgW",reviewService.getAverageWorkload(id));
-        session.setAttribute("avgTQ",reviewService.getAverageTeachingQuality(id));
-        session.setAttribute("avgCM",reviewService.getAverageCourseMaterial(id));
+        session.setAttribute("avgOAS", reviewService.getAverageOverallScore(id));
+        session.setAttribute("avgD", reviewService.getAverageDifficulty(id));
+        session.setAttribute("avgW", reviewService.getAverageWorkload(id));
+        session.setAttribute("avgTQ", reviewService.getAverageTeachingQuality(id));
+        session.setAttribute("avgCM", reviewService.getAverageCourseMaterial(id));
         List<Review> reviewSearchResults = reviewService.findByCourse_Name(((Course) session.getAttribute("selectedCourse")).getName());
         session.setAttribute("reviewsForCourse", reviewSearchResults);
         return "viewCourse";
