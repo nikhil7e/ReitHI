7637da1e9226714080ef94046304c097f790712f
tomaskri <98184013+tomaskri@users.noreply.github.com>
2022-11-13 16:42:30 +0000

    styling filters and restrictions added to empty fields

--- src/main/java/is/hi/hbv501g/reithi/Controllers/ViewCourseController.java
+++ src/main/java/is/hi/hbv501g/reithi/Controllers/ViewCourseController.java
@@ -51,14 +51,13 @@ public class ViewCourseController {
         }
 
         if (!removed){
-            review.addUpvote(currentUser);
-
             for (int i = 0; i<review.getDownvoters().size(); i++){
                 if(review.getDownvoters().get(i).getUserName().equals(currentUser.getUserName())){
                     review.removeDownvote(review.getDownvoters().get(i));
                     break;
                 }
             }
+            review.addUpvote(currentUser);
         }
         reviewService.save(review);
         refreshViewCourse(session, ((Course) session.getAttribute("selectedCourse")).getID());
@@ -80,13 +79,13 @@ public class ViewCourseController {
         }
 
         if (!removed){
-            review.addDownvote(currentUser);
             for (int i = 0; i<review.getUpvoters().size(); i++){
                 if(review.getUpvoters().get(i).getUserName().equals(currentUser.getUserName())){
                     review.removeUpvote(review.getUpvoters().get(i));
                     break;
                 }
             }
+            review.addDownvote(currentUser);
         }
 
         reviewService.save(review);
@@ -107,7 +106,13 @@ public class ViewCourseController {
         List<Review> allReviews = user.getReviews();
         allReviews.remove(review);
         user.setReviews(allReviews);
-
+        for (int i = 0; i<review.getUpvoters().size(); i++){
+            review.removeUpvote(review.getUpvoters().get(i));
+        }
+        for (int i = 0; i<review.getDownvoters().size(); i++){
+            review.removeDownvote(review.getDownvoters().get(i));
+        }
+        reviewService.save(review);
         reviewService.delete(review);
         session.setAttribute("hasReviewedCourse", false);
         refreshViewCourse(session, ((Course) session.getAttribute("selectedCourse")).getID());
--- src/main/resources/static/scripts/filter.js
+++ src/main/resources/static/scripts/filter.js
@@ -17,22 +17,22 @@ function hide() {
         const teachingQuality = parseFloat(course.querySelector(".teachingQuality").textContent.slice(18,21));
         const courseMaterial = parseFloat(course.querySelector(".courseMaterial").textContent.slice(17,20));
 
-        const creditFilterMax = parseInt(document.querySelector("#creditsMaxSpan").textContent);
-        const creditFilterMin = parseInt(document.querySelector("#creditsMinSpan").textContent);
+        const creditFilterMax = parseInt(document.querySelector(".creditsSlideMax").value);
+        const creditFilterMin = parseInt(document.querySelector(".creditsSlideMin").value);
         const graduateFilter = document.querySelector("#graduate").checked;
         console.log(graduateFilter);
         const undergraduateFilter = document.querySelector("#underGraduate").checked;
         console.log(undergraduateFilter);
-        const overallFilterMax = parseInt(document.querySelector("#overallMaxSpan").textContent);
-        const overallFilterMin = parseInt(document.querySelector("#overallMinSpan").textContent);
-        const difficultyFilterMax = parseInt(document.querySelector("#difficultyMaxSpan").textContent);
-        const difficultyFilterMin = parseInt(document.querySelector("#difficultyMinSpan").textContent);
-        const workloadFilterMax = parseInt(document.querySelector("#workloadMaxSpan").textContent);
-        const workloadFilterMin = parseInt(document.querySelector("#workloadMinSpan").textContent);
-        const teachingQualityFilterMax = parseInt(document.querySelector("#teachingQualityMaxSpan").textContent);
-        const teachingQualityFilterMin = parseInt(document.querySelector("#teachingQualityMinSpan").textContent);
-        const courseMaterialFilterMax = parseInt(document.querySelector("#courseMaterialMaxSpan").textContent);
-        const courseMaterialFilterMin = parseInt(document.querySelector("#courseMaterialMinSpan").textContent);
+        const overallFilterMax = parseInt(document.querySelector(".overallSlideMax").value);
+        const overallFilterMin = parseInt(document.querySelector(".overallSlideMin").value);
+        const difficultyFilterMax = parseInt(document.querySelector(".difficultySlideMax").value);
+        const difficultyFilterMin = parseInt(document.querySelector(".difficultySlideMin").value);
+        const workloadFilterMax = parseInt(document.querySelector(".workloadSlideMax").value);
+        const workloadFilterMin = parseInt(document.querySelector(".workloadSlideMin").value);
+        const teachingQualityFilterMax = parseInt(document.querySelector(".teachingQualitySlideMax").value);
+        const teachingQualityFilterMin = parseInt(document.querySelector(".teachingQualitySlideMin").value);
+        const courseMaterialFilterMax = parseInt(document.querySelector(".courseMaterialSlideMax").value);
+        const courseMaterialFilterMin = parseInt(document.querySelector(".courseMaterialSlideMin").value);
 
         course.removeAttribute("hidden")
 
@@ -58,7 +58,7 @@ function hide() {
 
         if (difficulty < difficultyFilterMin) {
             hide = true;
-            console.log("true")
+            console.log("true "+ difficultyFilterMin)
         }
         if (difficulty > difficultyFilterMax) {
             hide = true;
@@ -106,25 +106,133 @@ function hide() {
 }
 
 function updateTextInput() {
-    document.getElementById("creditsMaxSpan").textContent = document.getElementById("creditsMax").value;
-    document.getElementById("creditsMinSpan").textContent = document.getElementById("creditsMin").value;
+    //document.getElementById("creditsMaxSpan").textContent = document.getElementById("creditsMax").value;
+    //document.getElementById("creditsMinSpan").textContent = document.getElementById("creditsMin").value;
 
-    document.getElementById("overallMaxSpan").textContent = document.getElementById("overallMax").value;
-    document.getElementById("overallMinSpan").textContent = document.getElementById("overallMin").value;
+    //document.getElementById("overallMaxSpan").textContent = document.getElementById("overallMax").value;
+    //document.getElementById("overallMinSpan").textContent = document.getElementById("overallMin").value;
 
-    document.getElementById("difficultyMaxSpan").textContent = document.getElementById("difficultyMax").value;
-    document.getElementById("difficultyMinSpan").textContent = document.getElementById("difficultyMin").value;
+    //document.getElementById("difficultyMaxSpan").textContent = document.getElementById("difficultyMax").value;
+    //document.getElementById("difficultyMinSpan").textContent = document.getElementById("difficultyMin").value;
 
-    document.getElementById("workloadMaxSpan").textContent = document.getElementById("workloadMax").value;
-    document.getElementById("workloadMinSpan").textContent = document.getElementById("workloadMin").value;
+    //document.getElementById("workloadMaxSpan").textContent = document.getElementById("workloadMax").value;
+    //document.getElementById("workloadMinSpan").textContent = document.getElementById("workloadMin").value;
 
-    document.getElementById("teachingQualityMaxSpan").textContent = document.getElementById("teachingQualityMax").value;
-    document.getElementById("teachingQualityMinSpan").textContent = document.getElementById("teachingQualityMin").value;
+    //document.getElementById("teachingQualityMaxSpan").textContent = document.getElementById("teachingQualityMax").value;
+    //document.getElementById("teachingQualityMinSpan").textContent = document.getElementById("teachingQualityMin").value;
 
-    document.getElementById("courseMaterialMaxSpan").textContent = document.getElementById("courseMaterialMax").value;
-    document.getElementById("courseMaterialMinSpan").textContent = document.getElementById("courseMaterialMin").value;
+    //document.getElementById("courseMaterialMaxSpan").textContent = document.getElementById("courseMaterialMax").value;
+    //document.getElementById("courseMaterialMinSpan").textContent = document.getElementById("courseMaterialMin").value;
 
     }
+
+
+function controlFromInput(fromSlider, fromInput, toInput, controlSlider) {
+    const [from, to] = getParsed(fromInput, toInput);
+    fillSlider(fromInput, toInput, '#C6C6C6', '#25daa5', controlSlider);
+    if (from > to) {
+        fromSlider.value = to;
+        fromInput.value = to;
+    } else {
+        fromSlider.value = from;
+    }
+}
+
+function controlToInput(toSlider, fromInput, toInput, controlSlider) {
+    const [from, to] = getParsed(fromInput, toInput);
+    fillSlider(fromInput, toInput, '#C6C6C6', '#25daa5', controlSlider);
+    setToggleAccessible(toInput);
+    if (from <= to) {
+        toSlider.value = to;
+        toInput.value = to;
+    } else {
+        toInput.value = from;
+    }
+}
+
+function controlFromSlider(fromSlider, toSlider, fromInput) {
+    const [from, to] = getParsed(fromSlider, toSlider);
+    fillSlider(fromSlider, toSlider, '#C6C6C6', '#25daa5', toSlider);
+    if (from > to) {
+        fromSlider.value = to;
+        fromInput.value = to;
+    } else {
+        fromInput.value = from;
+    }
+}
+
+function controlToSlider(fromSlider, toSlider, toInput) {
+    const [from, to] = getParsed(fromSlider, toSlider);
+    fillSlider(fromSlider, toSlider, '#C6C6C6', '#25daa5', toSlider);
+    setToggleAccessible(toSlider);
+    if (from <= to) {
+        toSlider.value = to;
+        toInput.value = to;
+    } else {
+        toInput.value = from;
+        toSlider.value = from;
+    }
+}
+
+function getParsed(currentFrom, currentTo) {
+    const from = parseInt(currentFrom.value, 10);
+    const to = parseInt(currentTo.value, 10);
+    return [from, to];
+}
+
+function fillSlider(from, to, sliderColor, rangeColor, controlSlider) {
+    const rangeDistance = to.max-to.min;
+    const fromPosition = from.value - to.min;
+    const toPosition = to.value - to.min;
+    controlSlider.style.background = `linear-gradient(
+      to right,
+      ${sliderColor} 0%,
+      ${sliderColor} ${(fromPosition)/(rangeDistance)*100}%,
+      ${rangeColor} ${((fromPosition)/(rangeDistance))*100}%,
+      ${rangeColor} ${(toPosition)/(rangeDistance)*100}%, 
+      ${sliderColor} ${(toPosition)/(rangeDistance)*100}%, 
+      ${sliderColor} 100%)`;
+}
+
+function setToggleAccessible(currentTarget) {
+    if (Number(currentTarget.value) <= 0 ) {
+        currentTarget.style.zIndex = "2";
+    } else {
+        currentTarget.style.zIndex = "0";
+    }
+}
+
+const fromSliders = document.querySelectorAll('.slideMin');
+const toSliders = document.querySelectorAll('.slideMax');
+console.log(fromSliders)
+console.log(toSliders)
+const fromInputs = document.querySelectorAll('.fromInput');
+const toInputs = document.querySelectorAll('.toInput');
+console.log(fromInputs)
+console.log(toInputs)
+
+for (let i = 0; i<fromSliders.length; i++){
+    console.log(i)
+    let fromSlider = fromSliders.item(i);
+    console.log(fromSlider)
+    let toSlider = toSliders.item(i);
+
+    let fromInput = fromInputs.item(i);
+    let toInput = toInputs.item(i);
+    fillSlider(fromSlider, toSlider, '#C6C6C6', '#25daa5', toSlider);
+    setToggleAccessible(toSlider);
+
+    fromSlider.oninput = () => controlFromSlider(fromSlider, toSlider, fromInput);
+    toSlider.oninput = () => controlToSlider(fromSlider, toSlider, toInput);
+
+    fromInput.oninput = () => controlFromInput(fromSlider, fromInput, toInput, toSlider);
+    toInput.oninput = () => controlToInput(toSlider, fromInput, toInput, toSlider);
+}
+
+
+
+
+
 
 updateTextInput();
 hide();
