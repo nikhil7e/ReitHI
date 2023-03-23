a95fb89fadd36560d7c8d3de9180d7c12c1f29b8
tomaskri <98184013+tomaskri@users.noreply.github.com>
2023-03-23 20:29:31 +0000

    fixed the filter rest stuff

--- src/main/java/is/hi/hbv501g/reithi/Controllers/Rest/CourseRESTController.java
+++ src/main/java/is/hi/hbv501g/reithi/Controllers/Rest/CourseRESTController.java
@@ -1,5 +1,9 @@
 package is.hi.hbv501g.reithi.Controllers.Rest;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import is.hi.hbv501g.reithi.Persistence.Entities.Course;
 import is.hi.hbv501g.reithi.Persistence.Entities.CourseRating;
 import is.hi.hbv501g.reithi.Persistence.Entities.CourseSpecification;
@@ -14,6 +18,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -46,15 +56,86 @@ public class CourseRESTController {
         return courseService.findByNameContainingIgnoreCase(name, page - 1);
     }
 
-    // FOR TESTING PURPOSES, ADJUST
-    @RequestMapping(value = "/api/filter", method = RequestMethod.GET)
-    public List<Course> filterGET(@RequestParam("name") String name) {
-        CourseSpecification spec =
-                new CourseSpecification(new SearchCriteria("name", ":", name));
-        CourseSpecification spec2 =
-                new CourseSpecification(new SearchCriteria("nrReviews", ">", "1"));
+    @RequestMapping(value = "/api/getcoursebyid", method = RequestMethod.GET)
+    public Course genericGET(@RequestParam("id") int id ) {
+        return courseService.findByID(id);
+    }
 
-        return courseService.findAll(Specification.where(spec).and(spec2), 0).toList();
+    // FOR TESTING PURPOSES, ADJUST
+    @RequestMapping(value = "/api/filter", method = RequestMethod.POST)
+    public Page<Course> filterPOST(@RequestBody Map<String, String> json, @RequestParam(defaultValue = "1") int page) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> map = mapper.readValue(json.get("filter"), Map.class);
+        boolean scores = false;
+        List<Specification<Course>> specs = new ArrayList<>();
+
+
+        Object undergrad = map.get("undergraduate");
+        if (undergrad!=null){
+            System.out.println("undergraduate wasn't null");
+            specs.add(new CourseSpecification(new SearchCriteria("level", ":", "undergraduate")));
+        }
+        Object grad = map.get("graduate");
+        if (grad!=null){
+            System.out.println("graduate wasn't null");
+            specs.add(new CourseSpecification(new SearchCriteria("level", "!:", "undergraduate")));
+        }
+        ArrayList<Integer> creditsRange = (ArrayList<Integer>) map.get("creditsRange");
+        if (creditsRange != null) {
+            specs.add(new CourseSpecification(new SearchCriteria("credits", "<", creditsRange.get(1))));
+            specs.add(new CourseSpecification(new SearchCriteria("credits", ">", creditsRange.get(0))));
+        }
+
+        ArrayList<Integer> overallRange = (ArrayList<Integer>) map.get("overallRange");
+        if (overallRange != null) {
+            specs.add(new CourseSpecification(new SearchCriteria("totalOverall", "sum<", overallRange.get(1))));
+            specs.add(new CourseSpecification(new SearchCriteria("totalOverall", "sum>", overallRange.get(0))));
+            specs.add(new CourseSpecification(new SearchCriteria("totalOverall", ">", 1)));
+        }
+
+        ArrayList<Integer> totalDifficultyRange = (ArrayList<Integer>) map.get("difficultyRange");
+        if (totalDifficultyRange != null) {
+            specs.add(new CourseSpecification(new SearchCriteria("totalDifficulty", "sum<", totalDifficultyRange.get(1))));
+            specs.add(new CourseSpecification(new SearchCriteria("totalDifficulty", "sum>", totalDifficultyRange.get(0))));
+            specs.add(new CourseSpecification(new SearchCriteria("totalDifficulty", ">", 1)));
+        }
+
+        ArrayList<Integer> totalWorkloadRange = (ArrayList<Integer>) map.get("workloadRange");
+        if (totalWorkloadRange != null) {
+            specs.add(new CourseSpecification(new SearchCriteria("totalWorkload", "sum<", totalWorkloadRange.get(1))));
+            specs.add(new CourseSpecification(new SearchCriteria("totalWorkload", "sum>", totalWorkloadRange.get(0))));
+            specs.add(new CourseSpecification(new SearchCriteria("totalWorkload", ">", 1)));
+        }
+
+        ArrayList<Integer> totalTeachingQualityRange = (ArrayList<Integer>) map.get("teachingQualityRange");
+        if (totalTeachingQualityRange != null) {
+            specs.add(new CourseSpecification(new SearchCriteria("totalTeachingQuality", "sum<", totalTeachingQualityRange.get(1))));
+            specs.add(new CourseSpecification(new SearchCriteria("totalTeachingQuality", "sum>", totalTeachingQualityRange.get(0))));
+            specs.add(new CourseSpecification(new SearchCriteria("totalTeachingQuality", ">", 1)));
+        }
+
+        ArrayList<Integer> totalCourseMaterialRange = (ArrayList<Integer>) map.get("courseMaterialRange");
+        if (totalCourseMaterialRange != null) {
+            specs.add(new CourseSpecification(new SearchCriteria("totalCourseMaterial", "sum<", totalCourseMaterialRange.get(1))));
+            specs.add(new CourseSpecification(new SearchCriteria("totalCourseMaterial", "sum>", totalCourseMaterialRange.get(0))));
+            specs.add(new CourseSpecification(new SearchCriteria("totalCourseMaterial", ">", 1)));
+        }
+        CourseSpecification spec2 = new CourseSpecification(new SearchCriteria("ID", ">", 0));
+
+
+        Specification<Course> combinedSpec = spec2;
+
+        for (Specification<Course> spec : specs) {
+            combinedSpec = combinedSpec.and(spec);
+        }
+
+
+
+        if (combinedSpec != spec2) {
+            return courseService.findAll(combinedSpec, page-1);
+        } else {
+            return courseService.findAll(page-1);
+        }
         //return courseService.findByNameContainingIgnoreCase(name, 20);
     }
 
--- src/main/java/is/hi/hbv501g/reithi/Persistence/Entities/CourseSpecification.java
+++ src/main/java/is/hi/hbv501g/reithi/Persistence/Entities/CourseSpecification.java
@@ -2,10 +2,7 @@ package is.hi.hbv501g.reithi.Persistence.Entities;
 
 import org.springframework.data.jpa.domain.Specification;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
+import javax.persistence.criteria.*;
 
 public class CourseSpecification implements Specification<Course> {
 
@@ -30,10 +27,31 @@ public class CourseSpecification implements Specification<Course> {
         else if (criteria.getOperation().equalsIgnoreCase(":")) {
             if (root.get(criteria.getKey()).getJavaType() == String.class) {
                 return builder.like(
-                        root.<String>get(criteria.getKey()), "%" + criteria.getValue() + "%");
+                        builder.function("LOWER", String.class, root.<String>get(criteria.getKey())),
+                        criteria.getValue().toString().toLowerCase() + "%");
             } else {
                 return builder.equal(root.get(criteria.getKey()), criteria.getValue());
             }
+        }
+        else if (criteria.getOperation().equalsIgnoreCase("!:")) {
+            return builder.notLike(
+                    builder.function("LOWER", String.class, root.<String>get(criteria.getKey())),
+                    criteria.getValue().toString().toLowerCase());
+
+        }
+        else if (criteria.getOperation().equalsIgnoreCase("sum<")) {
+            int max = (int) criteria.getValue();
+            System.out.println("Less than " + max);
+            Expression<Integer> nrReviews = root.get("nrReviews");
+            return builder.lessThanOrEqualTo(root.get(criteria.getKey()),
+                    builder.prod(builder.literal(max), nrReviews));
+        }
+        else if (criteria.getOperation().equalsIgnoreCase("sum>")) {
+            int min = (int) criteria.getValue();
+            System.out.println("Greater than " + min);
+            Expression<Integer> nrReviews = root.get("nrReviews");
+            return builder.greaterThanOrEqualTo(root.get(criteria.getKey()),
+                    builder.prod(builder.literal(min), nrReviews));
         }
         return null;
     }
